1b7b593877d39d7533e153b3099a3e125f874371
AndersonClayton <andersonclaytondesenvolvedor@gmail.com>
2022-05-23 15:43:25 -0300

    Efeitos colaterais (useEffect) em React

--- react-hooks/src/Components/Ifoodcounter/Ifoodcounter.js
+++ react-hooks/src/Components/Ifoodcounter/Ifoodcounter.js
@@ -1,10 +1,15 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import '../Ifoodcounter/Ifoodcounter.css'
 
 export default function Ifoodcounter() {
 
     const [value, setValue] = useState(1);
     const [buttonStyle, setButtonStyle] = useState("counter-button-minus-active")
+    
+    useEffect(() => {
+        //alert('Você não pode mais diminuir itens');
+        console.log(`o estilo do botão atual é: ${buttonStyle}` );
+    },[buttonStyle])
 
     function down() {
 
@@ -27,6 +32,7 @@ export default function Ifoodcounter() {
         <button className={buttonStyle} onClick={down}>-</button>
         <p>{value}</p>
         <button className='counter-button-plus-active' onClick={up}>+</button>
+        <button id='moeda'>12,00</button>
     </div>
   )
 }
